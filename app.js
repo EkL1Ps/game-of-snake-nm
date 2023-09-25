e10db246cc24ac2fa363e1814887975c1aef7fc7
Ekuripusu <dogasshole.test@gmail.com>
2023-09-25 17:32:20 +0300

    finished logic of pause

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ snakesHead.src = "images/logo.png";
 const snakesBody = new Image();
 snakesBody.src = "images/snakes-body.png";
 
+isPaused = false;
+
 let box = 32;
 let score = 0;
 
@@ -35,7 +37,15 @@ function direction(event) {
   else if (event.keyCode == 38 && dir != "down") dir = "up";
   else if (event.keyCode == 39 && dir != "left") dir = "right";
   else if (event.keyCode == 40 && dir != "up") dir = "down";
-  else if (event.keyCode == 32) dir = "space";
+  else if (event.keyCode == 32) {
+    if (isPaused) {
+      isPaused = false;
+      game = setInterval(drawGame, 70);
+    } else {
+      isPaused = true;
+      clearInterval(game);
+    }
+  }
 }
 
 function youWin() {
@@ -57,74 +67,70 @@ function eatBody(head, arr) {
 }
 
 function drawGame() {
-  ctx.drawImage(ground, 0, 0);
+  if (!isPaused) {
+    ctx.drawImage(ground, 0, 0);
 
-  ctx.drawImage(foodImg, food.x, food.y);
+    ctx.drawImage(foodImg, food.x, food.y);
 
-  for (let i = 0; i < snake.length; i++) {
-    ctx.drawImage(
-      i == 0 ? snakesHead : snakesBody,
-      snake[i].x,
-      snake[i].y,
-      box,
-      box
-    );
-  }
-  for (let i = 1; i < snake.length; i++) {
-    bodySegments.push({ x: snake[i].x, y: snake[i].y });
-  }
-  function drawBody() {
-    for (let i = 0; i < bodySegments.length; i++) {
-      ctx.drawImage(snakesBody, bodySegments[i].x, bodySegments[i].y, box, box);
+    for (let i = 0; i < snake.length; i++) {
+      ctx.drawImage(
+        i == 0 ? snakesHead : snakesBody,
+        snake[i].x,
+        snake[i].y,
+        box,
+        box
+      );
     }
-  }
 
-  ctx.fillStyle = "white";
-  ctx.font = "50px Roboto";
-  ctx.fillText(score, box * 2.5, box * 1.7);
+    ctx.fillStyle = "white";
+    ctx.font = "50px Roboto";
+    ctx.fillText(score, box * 2.5, box * 1.7);
 
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
-  //score counter
-  if (snakeX == food.x && snakeY == food.y) {
-    score++;
-    food = {
-      x: Math.floor(Math.random() * 17 + 1) * box,
-      y: Math.floor(Math.random() * 15 + 3) * box,
+    //score counter
+    if (snakeX == food.x && snakeY == food.y) {
+      score++;
+      food = {
+        x: Math.floor(Math.random() * 17 + 1) * box,
+        y: Math.floor(Math.random() * 15 + 3) * box,
+      };
+    } else {
+      snake.pop();
+    }
+
+    if (
+      snakeX < box ||
+      snakeX > box * 17 ||
+      snakeY < 3 * box ||
+      snakeY > box * 17
+    ) {
+      clearInterval(game);
+      alert(`Game Over! Your finalscore: ${score}; Press f5 to restart`);
+    }
+
+    if (dir == "left") snakeX -= box;
+    if (dir == "right") snakeX += box;
+    if (dir == "up") snakeY -= box;
+    if (dir == "down") snakeY += box;
+    if (dir == "space") {
+      clearInterval(drawGame);
+      pausedSnakeHead = { x: snakeX, y: snakeY };
+      pausedSnakeBody = bodySegments.slice();
+    }
+
+    let newBody = {
+      x: snakeX,
+      y: snakeY,
     };
-  } else {
-    snake.pop();
+
+    eatBody(newBody, snake);
+    youWin();
+
+    snake.unshift(newBody);
+    bodySegments.unshift({ x: snakeX, y: snakeY });
   }
-
-  if (
-    snakeX < box ||
-    snakeX > box * 17 ||
-    snakeY < 3 * box ||
-    snakeY > box * 17
-  ) {
-    clearInterval(game);
-    alert(`Game Over! Your finalscore: ${score}; Press f5 to restart`);
-  }
-
-  if (dir == "left") snakeX -= box;
-  if (dir == "right") snakeX += box;
-  if (dir == "up") snakeY -= box;
-  if (dir == "down") snakeY += box;
-  if (dir == "space") {
-    clearInterval(drawGame);
-    clearInterval(drawBody);
-  }
-
-  let newBody = {
-    x: snakeX,
-    y: snakeY,
-  };
-
-  eatBody(newBody, snake);
-  youWin();
-
-  snake.unshift(newBody);
 }
 
 let game = setInterval(drawGame, 70);
